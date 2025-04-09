7b398b4db611cda94babeca70e9903b4b76daf1c
Nahuel Diaz <nahueldiaz92@gmail.com>
2025-04-09 12:45:29 -0300

    feat: add basic AuthController and database conection config

---
+++ src/main/java/com/conexa/starwars/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.conexa.starwars.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping
+    public String showAuthChoice() {
+        return "auth/auth-choice";
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "auth/login";
+    }
+
+    @GetMapping("/register")
+    public String showRegisterForm() {
+        return "auth/register";
+    }
+}
